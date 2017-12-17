221961496f2f838c5d2c41478568d545c7efa85d
vahrameloyan <vahrameloyan@gmail.com>
2017-12-17 13:03:39 +0400

    Added new mvc application

---
+++ Sprint1.Solution/Sprint1.Solution/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sprint1.Solution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
